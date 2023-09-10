1c4ed0c1aff6e83fecbea2e690834de686238eb3
yolomiyaba <ac3po6@gmail.com>
2023-09-10 20:47:23 +0900

    memo app

--- 04_ゼミ/seminar1-3/main.js
+++ 04_ゼミ/seminar1-3/main.js
@@ -2,20 +2,45 @@ const memolist = document.getElementById("memolist")
 const saveButton = document.getElementById("input-button")
 const inputField = document.getElementById("input-text")
 
-let lol = ""
+let text = ""
+let localMemo = localStorage.getItem("memo")
+let memo = []
+if (localMemo) {
+  memo = JSON.parse(localStorage.memo)
+  console.log(memo)
+  for (let i = 1; i < memo.length; i++) {
+    console.log(memo[i])
+    text = memo[i]
+    createMemo
+  }
+}
+
 inputField.onchange = function (e) {
-  lol = e.target.value
-  console.log(lol)
+  text = e.target.value
+  console.log(text)
 }
 
 saveButton.onclick = function () {
-  console.log(lol)
+  console.log(text)
   const card = document.createElement("div")
   card.id = "memocard"
 
   const content = document.createElement("div")
   content.id = "cardcontent"
-  content.textContent = lol
+  content.textContent = text
+  card.appendChild(content)
+  memolist.appendChild(card)
+  memo.push(text)
+  localStorage.memo = JSON.stringify(memo)
+}
+
+const createMemo = function () {
+  const card = document.createElement("div")
+  card.id = "memocard"
+
+  const content = document.createElement("div")
+  content.id = "cardcontent"
+  content.textContent = text
   card.appendChild(content)
   memolist.appendChild(card)
 }
